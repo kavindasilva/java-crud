91f5c51efd168e919cce54e05c011909a0e80b19
kavindasilva <kss.kavinda@gmail.com>
2020-02-16 12:14:06 +0530

    wip get bill_item by item id

--- src/main/java/com/example/javabill/BillItem.java
+++ src/main/java/com/example/javabill/BillItem.java
@@ -10,16 +10,42 @@ import org.hibernate.annotations.OnDeleteAction;
 import org.hibernate.annotations.SQLInsert;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
 @Entity // This tells Hibernate to make a table out of this class
 @Table(name = "bill_item")
-public class BillItem {
+//public class BillItem implements Serializable {
+public class BillItem{
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Integer bill_item_id;
-    private String item_batch;
+//    private String item_batch;
     private int bill_id; // fk
     private Double qty;
+
+    @ManyToOne()
+    @JoinColumn(name="item", insertable = false, updatable = false, referencedColumnName = "item_id")
+    private Item item;
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name="item_batch", insertable = false, updatable = false, referencedColumnName = "item_batch")
+//    private Item item_batch;
+
+//    public Item getItemData() {
+//        return itemData;
+//    }
+//    public void setItemData(Item itemData) {
+//        this.itemData = itemData;
+//    }
+
+
+
+    public Item getItem() {
+        return item;
+    }
+    public void setItem(Item item) {
+        this.item = item;
+    }
 
     public Integer getBill_item_id() {
         return bill_item_id;
@@ -28,12 +54,12 @@ public class BillItem {
         this.bill_item_id = id;
     }
 
-    public String getItem_batch() {
-        return item_batch;
-    }
-    public void setItem_batch(String item_batch) {
-        this.item_batch = item_batch;
-    }
+//    public Item getItem_batch() {
+//        return item_batch;
+//    }
+//    public void setItem_batch(Item item_batch) {
+//        this.item_batch = item_batch;
+//    }
 
     public Double getQty() {
         return qty;
