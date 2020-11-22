66e39db04d6ed9e43089fe1ccc2c6b76d3fb78d5
kavindasilva <kss.kavinda@gmail.com>
2020-11-22 19:56:31 +0530

    user endpoint GET

--- src/main/java/com/example/rentsystem/UserController.java
+++ src/main/java/com/example/rentsystem/UserController.java
@@ -7,32 +7,28 @@ import org.springframework.web.bind.annotation.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Iterator;
+import java.util.Optional;
 
 @Controller // This means that this class is a Controller
-@RequestMapping(path="/") // This means URL's start with /demo (after Application path)
 public class UserController {
     @Autowired
     private UserRepository userRepository;
-//    @Autowired
-//    private ItemRepository itemRepository;
-//    @Autowired // This means to get the bean called userRepository
-//    private BillItemRepository billItemRepository;
-
 
 //    @PostMapping(path="/bill") // POST /user  ;  Map ONLY POST Requests
 //    public @ResponseBody String createNewBill (@RequestBody Bill newBill) {
 //        return "";
 //    }
 
-    @GetMapping(path="/users") // GET /user
+    @GetMapping(path="/users") // GET /users
     public @ResponseBody Iterable<User> getAllUsers() {
         // This returns a JSON or XML with the users
         return userRepository.findAll();
     }
-//    @GetMapping(path="/billItems") // GET /user
-//    public @ResponseBody Iterable<BillItem> getAllBillItems() {
-//        // This returns a JSON or XML with the users
-//        return billItemRepository.findAll();
-//    }
+
+    @GetMapping(path="/user/{id}") // GET user/:id
+    public @ResponseBody Optional<User> getItemByUrlId(@PathVariable int id ) {
+        return userRepository.findById(id);
+    }
+
 
 }
