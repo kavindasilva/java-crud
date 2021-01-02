b79357500c2a007abbc3fb7742413b07553b0265
kavindasilva <kss.kavinda@gmail.com>
2021-01-02 16:53:13 +0530

    WIP UserController testing

--- src/test/java/com/example/user/UserEndpointTests.java
+++ src/test/java/com/example/user/UserEndpointTests.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -14,15 +15,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-//import com.example.security.SecurityConstants.*;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.Matchers.containsString;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
@@ -33,7 +33,7 @@ public class UserEndpointTests {
     protected static final String URL_USER_GET_ALL_RESTRICTED = "/users/allu";
     protected static final String URL_USER_LOGIN = "/login";
 
-    // @TODO: find a better way to decvlare the JSON
+    // @TODO: find a better way to declare the JSON
     protected static final String USER_VALID_CREDS = "{ \"name\": \"web2\", \"password\": \"password\" }";
     protected static final String USER_INVALID_CREDS = "{ \"name\": \"web2invalid\", \"password\": \"password\" }";
 //    @LocalServerPort
@@ -45,19 +45,6 @@ public class UserEndpointTests {
     @Autowired
     private MockMvc mockMvc;
 
-//    protected String getCredentails(boolean isValid){
-////        ObjectMapper objMapper = new ObjectMapper();
-////        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
-//        try {
-//            JSONObject jsonObject;
-////            if(isValid)
-//            jsonObject = (isValid) ? new JSONObject(this.USER_VALID_CREDS) : new JSONObject(this.USER_INVALID_CREDS);
-//        }catch (JSONException err){
-//            System.out.println("Error: " + err.toString());
-//        }
-//        return
-//    }
-////
     @Test
     public void shouldShowUnauthorizedMessage() throws Exception {
         this.mockMvc.perform(get(this.URL_USER_GET_ALL)).andDo(print()).andExpect(status().isForbidden())
@@ -66,13 +53,30 @@ public class UserEndpointTests {
 
     @Test
     public void tryLoginWithBadCredentials() throws Exception {
-        this.mockMvc.perform(
-                    post(this.URL_USER_LOGIN )
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .content( this.USER_INVALID_CREDS )
-                )
-                .andDo(print()).andExpect(status().isUnauthorized())
-                .andExpect( content().string("") );
+        MvcResult res = this.mockMvc.perform(
+                post(this.URL_USER_LOGIN )
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content( this.USER_INVALID_CREDS )
+                ).andDo(print()).andExpect(status().isUnauthorized()).andReturn();
+
+        Assertions.assertEquals( 401, res.getResponse().getStatus() );
+        Assertions.assertTrue( res.getResponse().containsHeader("Reason-Failed") );
+        Assertions.assertFalse( res.getResponse().containsHeader("Authorization") );
+        Assertions.assertEquals( "Bad credentials", res.getResponse().getHeader("Reason-Failed") );
+    }
+
+    @Test
+    public void tryLoginWithCorrectCredentials() throws Exception {
+        MvcResult res = this.mockMvc.perform(
+                post(this.URL_USER_LOGIN )
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content( this.USER_VALID_CREDS )
+        ).andDo(print()).andExpect(status().is2xxSuccessful()).andReturn();
+
+        Assertions.assertEquals( 200, res.getResponse().getStatus() );
+        Assertions.assertFalse( res.getResponse().containsHeader("Reason-Failed") );
+        Assertions.assertTrue( res.getResponse().containsHeader("Authorization") );
+        Assertions.assertEquals( "Bearer", res.getResponse().getHeader("Authorization").split(" ")[0] );
     }
 
 //    @Test
